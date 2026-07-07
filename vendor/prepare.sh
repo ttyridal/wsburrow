6ffe7ddc913cb29778556ade390584eef6903610
Torbjorn Tyridal <torbjorn@tyridal.no>
2026-07-07 21:54:59 +0000

    openwrt makefile

---
+++ vendor/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+curl -o libwebsockets-4.5.8.zip  https://github.com/warmcat/libwebsockets/archive/refs/tags/v4.5.8.zip
+
+git init libubox
+cd libubox
+git remote add origin https://github.com/openwrt/libubox.git
+git fetch --depth 1 origin 1fe93d2fefb213ec987763e7e94ce5eaa757bfc3
+patch -p1 < ../01-libublox-build-convenience.patch
+
+
